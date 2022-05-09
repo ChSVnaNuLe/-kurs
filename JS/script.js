4a1454c2bf86a95f083c900355346098f6641d8c
ChSVnaNuLe <96132520+ChSVnaNuLe@users.noreply.github.com>
2022-05-09 23:56:15 +0300

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+var i = 0;
+var images =[];
+var time = 5000;
+images[0] = "D:/Курсач/Сайт/IMG/slaid1.jpg";
+images[1] = "D:/Курсач/Сайт/IMG/slaid2.jpg";
+images[2] = "D:/Курсач/Сайт/IMG/slaid3.jpg";
+images[3] = "D:/Курсач/Сайт/IMG/slaid4.jpg";
+function slideShow(){
+  document.slideshow.src = images[i];
+  if (i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("slideShow()", time);
+}
+window.onload = slideShow;
